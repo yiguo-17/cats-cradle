6fc8d75f30a6d65b840f021784b7149ff88c2fe3
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-06-24 23:48:46 -0400

    add functions and add break for function 2.

--- main.js
+++ main.js
@@ -2,30 +2,108 @@ function challengeBanner(num){
   console.log('######### Challenge '+ num + ' #########');
 }
 
-function printvow(str){
+function printVowel(str){
   challengeBanner(1);
-  const vow = 'aeiou';
+  const vowel = 'aeiou';
   const len = str.length;
   let i = 0;
   while(i < len){
-    if(vow.includes(str[i])){console.log(str[i]);}
+    if(vowel.includes(str[i])){console.log(str[i]);}
     i++
   }
 }
-printvow("Regular expressions are for term 2.")
+printVowel("Regular expressions are for term 2.")
 
-function fivevow(str){
+function fiveVowel(str){
   challengeBanner(2);
-  const vow = 'aeiou';
+  const vowel = 'aeiou';
   const len = str.length;
-  let i = 0;
-  let j = 0;
+  let i = 0; //counter for string
+  let j = 0; //counter for vowels
   while(i < len){
-    while(j <= 5){
-       if(vow.includes(str[i])){console.log(str[i]);j++;}
-       i++;
+       if(vowel.includes(str[i])){
+          console.log(str[i]);
+          j++;
+       } 
+       i++; 
+      if(j ===5){break}
     }
   }
-  
+fiveVowel("Regular expressions are for term 2.")
+fiveVowel("Hello!")
+
+function everyThird(str){
+  challengeBanner(3);
+  const len = str.length;
+  let i = 2;
+  while(i < len){
+    console.log(str[i]);
+    i = i + 3;
+  }
 }
-fivevow("Regular expressions are for term 2.")
+everyThird("I am the alfalfa and the omelette.");
+
+function fourFrom(str,num){
+  challengeBanner(4);
+  let i = num
+  const len = str.length;
+  if((num + 4) > len){
+    while(i < len){
+      console.log(str[i]);
+      i++;
+    }
+  }
+  else{while(i < (num+4)){
+    console.log(str[i]);
+    i++;
+  }
+
+  }
+}
+const notice = "Oh hi, I didn't see you there. Welcome."
+fourFrom(notice, 4);
+fourFrom(notice, 36)
+
+function whereIsU(str){
+  challengeBanner(5);
+  let i = 0;
+  const len = str.length;
+  while (i < len){
+    if(str[i] === 'u'){console.log(i)}
+    i++;
+  }
+}
+
+const argue = 'You picked the wrong house, bub.';
+whereIsU(argue)
+
+function firstU(str){
+  challengeBanner(6);
+  let i = 0;
+  let us = '';
+  const len = str.length;
+  while (i < len){
+    if(str[i] === 'u'){us = us + i}
+    i++;
+  }
+  console.log(us[0]);
+}
+
+firstU(argue)
+
+function uOrNothing(str){
+  challengeBanner(7);
+  let i = 0;
+  let us = '';
+  const len = str.length;
+  while (i < len){
+    if(str[i] === 'u'){us = us + i}
+    i++;
+  }
+  if(us === ''){console.log(-1);}
+  else {console.log(us[0]);}
+}
+
+uOrNothing(argue)
+const nation = "I'm Canadian.";
+uOrNothing(nation) ;
